f4ade0272e03c9f2d7be7ea6e0c9483ef79764d1
vaibhavupadhyay6 <vaibhavupadhyay698@gmail.com>
2023-10-31 20:16:20 +0530

    update

---
+++ lib/screens/home/components/profilePic.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 50,
+      width: 50,
+      child: Stack(
+        fit: StackFit.expand,
+        clipBehavior: Clip.none,
+        children: [
+          CircleAvatar(
+            backgroundImage: AssetImage("assets/images/Profile Image.png"),
+          ),
+        ],
+      ),
+    );
+  }
+}
